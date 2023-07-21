6c7a8f747b4612f2a1c008cfd8f81be55adc0f56
codewitgabi <gabrielmicheal497@gmail.com>
2023-07-21 08:59:58 +0100

    Ready for dsployment

--- urlshortener/wsgi.py
+++ urlshortener/wsgi.py
@@ -14,3 +14,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'urlshortener.settings')
 
 application = get_wsgi_application()
+
+
+# vercel config
+
+app = application
